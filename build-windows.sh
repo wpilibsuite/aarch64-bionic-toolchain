ab623cae56bd03cb720856d08f441baf0b80b8e0
Thad House <thadhouse1@gmail.com>
2019-04-29 20:51:57 -0700

    Rename nano to bionic

--- build-windows.sh
+++ build-windows.sh
@@ -14,5 +14,5 @@ docker pull wpilib/toolchain-builder:18.04 \
   && make gcc \
   && sudo cp gcc-install/usr/local/* /usr/local/ -r \
   && cd ../windows \
-  && env PATH=\${PATH}:/usr/local/nano1804-toolchain/bin make sysroot binutils gcc gdb tree zip \
+  && env PATH=\${PATH}:/usr/local/bionic-toolchain/bin make sysroot binutils gcc gdb tree zip \
   && cp *.zip /artifacts/"
