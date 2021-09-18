facd3c4403db74a06b70365f860a1e1d03f678af
Peter Johnson <johnson.peter@gmail.com>
2021-09-18 13:47:55 -0700

    Add Mac codesign

---
+++ codesign.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+source versions.sh
+
+sign_directory()
+{
+    find $1 | while read fname; do
+        if [[ -f $fname ]]; then
+            echo "[INFO] Signing $fname"
+            codesign --force --strict --timestamp --options=runtime -s $2 $fname
+        fi
+    done
+}
+
+cd mac
+tar -pxvzf FRC-${V_YEAR}-Mac-Toolchain-${V_GCC}.tar.gz
+sign_directory "bionic/bin" $1
+sign_directory "bionic/arm-linux-gnueabihf/bin" $1
+sign_directory "bionic/libexec/gcc/arm-linux-gnueabihf/${V_GCC}" $1
+tar -pcvzf Bionic-Mac-Toolchain-${V_GCC}.tar.gz bionic
+rm -rf bionic
