6ae1ba34252190cbc1f82afb6567772e210241a6
Peter Johnson <johnson.peter@gmail.com>
2021-11-10 20:19:54 -0800

    Fix versions and downloads

--- download.sh
+++ download.sh
@@ -1,16 +1,16 @@
 #!/bin/zsh
 source versions.sh
 
-wget -nc -nv \
+wget -nc -nv --no-check-certificate \
     https://ftp.gnu.org/gnu/gcc/gcc-${V_GCC}/gcc-${V_GCC}.tar.gz \
     https://ftp.gnu.org/gnu/binutils/binutils-${V_BIN}.tar.bz2 \
     https://ftp.gnu.org/gnu/mpfr/mpfr-${V_MPFR}.tar.bz2 \
     https://ftp.gnu.org/gnu/mpc/mpc-${V_MPC}.tar.gz \
     https://ftp.gnu.org/gnu/gmp/gmp-${V_GMP}.tar.bz2 \
     http://www.bastoul.net/cloog/pages/download/cloog-${V_CLOOG}.tar.gz \
-    http://isl.gforge.inria.fr/isl-${V_ISL}.tar.bz2 \
+    https://gcc.gnu.org/pub/gcc/infrastructure/isl-${V_ISL}.tar.bz2 \
     https://ftp.gnu.org/gnu/gdb/gdb-${V_GDB}.tar.gz \
-    https://sourceforge.net/projects/expat/files/expat/${Vw_EXPAT}/expat-${Vw_EXPAT}-RENAMED-VULNERABLE-PLEASE-USE-2.3.0-INSTEAD.tar.bz2 \
+    https://sourceforge.net/projects/expat/files/expat/${Vw_EXPAT}/expat-${Vw_EXPAT}.tar.bz2 \
     http://ports.ubuntu.com/ubuntu-ports/pool/main/g/gcc-8/libgcc1_${Va_LIBGCC}_arm64.deb \
     http://ports.ubuntu.com/ubuntu-ports/pool/main/g/gcc-8/libgcc1-dbg_${Va_LIBGCC}_arm64.deb \
     http://ports.ubuntu.com/ubuntu-ports/pool/main/g/gcc-8/libgcc-8-dev_${Va_LIBGCC}_arm64.deb \
@@ -30,5 +30,3 @@ wget -nc -nv \
     http://ports.ubuntu.com/ubuntu-ports/pool/main/g/glibc/libc6-dev_${Va_LIBC}_arm64.deb \
     http://ports.ubuntu.com/ubuntu-ports/pool/main/l/linux/linux-libc-dev_${Va_LINUX}_arm64.deb
 
-mv expat-${Vw_EXPAT}-RENAMED-VULNERABLE-PLEASE-USE-2.3.0-INSTEAD.tar.bz2 \
-    expat-${Vw_EXPAT}.tar.bz2
