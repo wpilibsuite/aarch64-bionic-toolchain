3dc1f954e2e18cfa5ccd096519aefaec6174f961
Peter Johnson <johnson.peter@gmail.com>
2021-11-10 22:00:13 -0800

    Fix codesign.sh

--- codesign.sh
+++ codesign.sh
@@ -12,7 +12,7 @@ sign_directory()
 }
 
 cd mac
-tar -pxvzf FRC-${V_YEAR}-Mac-Toolchain-${V_GCC}.tar.gz
+tar -pxvzf Bionic-Mac-Toolchain-${V_GCC}.tar.gz
 sign_directory "bionic/bin" $1
 sign_directory "bionic/arm-linux-gnueabihf/bin" $1
 sign_directory "bionic/libexec/gcc/arm-linux-gnueabihf/${V_GCC}" $1
